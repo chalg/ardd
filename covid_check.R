7e146abd813404e08cae8e3e9eb23a68548774bb
Grant <grantchalmers@yahoo.com>
2021-01-28 18:46:25 +1000

    update to dec 2020

--- covid_check.R
+++ covid_check.R
@@ -2,7 +2,7 @@
 # Nothing noticeable.
 # Overall
 fatal_unique %>%
-  filter(month == 3) %>% 
+  filter(month == 4) %>% 
   group_by(year, month) %>% 
   summarise(sum_fatal_state_yr = sum(number_fatalities)) %>%
   ggplot(aes(x = year, y = sum_fatal_state_yr)) +
@@ -12,7 +12,7 @@ fatal_unique %>%
   scale_x_continuous(breaks = seq(1989, 2020, 1), limits = c(1989, 2020)) + 
   # facet_wrap( ~ state, scales = "free_y", ncol = 2) +
   labs(x = "Year", y = "Number of Fatalities",
-       title = 'Australian Road Fatalities in March (1989-2020)',
+       title = 'Australian Road Fatalities in April (1989-2020)',
        # subtitle = "Month of March totals",
        caption = "Source: @GrantChalmers | Australian Road Deaths Database (ARDD)") +
   theme(plot.title = element_text(size = 12, face = "bold"),
@@ -23,7 +23,7 @@ fatal_unique %>%
         plot.caption = element_text(size = 9, color = "gray50", face = "italic"),
         legend.title = element_text(size = 9), legend.text = element_text(size = 9))
 
-ggsave("ardd_march_totals.png", path = "images", width = 6, height = 4)
+ggsave("ardd_april_totals.png", path = "images", width = 6, height = 4)
 
 # Will be very interesting to look at the easter_period next month as the
 # categorical fill variable!!
@@ -33,7 +33,8 @@ my_colours <- c("Single"     = "gold2",
                 "Multiple"   = "#CC79A7")
 
 fatal_unique %>%
-  filter(month == 3) %>% 
+  filter(month == 12) %>% 
+         # easter_period == "Yes") %>% 
   group_by(year, month, crash_type) %>% 
   summarise(sum_fatal_yr = sum(number_fatalities)) %>%# view()
   ggplot(aes(x = year, y = sum_fatal_yr, fill = crash_type)) +
@@ -45,7 +46,7 @@ fatal_unique %>%
   scale_fill_manual(values = my_colours) +
   # facet_wrap( ~ state, scales = "free_y", ncol = 2) +
   labs(x = "Year", y = "Number of Fatalities",
-       title = 'Australian Road Fatalities in March (1989-2020)',
+       title = 'Australian Road Fatalities in December (1989-2020)',
        fill = "Crash type",
        # subtitle = "Month of March totals",
        caption = "Source: @GrantChalmers | Australian Road Deaths Database (ARDD)") +
@@ -58,10 +59,10 @@ fatal_unique %>%
         plot.caption = element_text(size = 9, color = "gray50", face = "italic"),
         legend.title = element_text(size = 9), legend.text = element_text(size = 9))
 
-ggsave("ardd_march_totals_crash_type.png", path = "images", width = 6, height = 4)
+ggsave("ardd_dec_totals_crash_type.png", path = "images", width = 6, height = 4)
 
 fatal_unique %>%
-  filter(month == 3) %>% 
+  filter(month == 4) %>% 
   group_by(year, month, state) %>% 
   summarise(sum_fatal_state_yr = sum(number_fatalities)) %>% 
   arrange(sum_fatal_state_yr) %>% 
@@ -69,7 +70,7 @@ fatal_unique %>%
 
 # By State
 fatal_unique %>%
-  filter(month == 3) %>% 
+  filter(month == 4) %>% 
   group_by(year, month, state) %>% 
   summarise(sum_fatal_state_yr = sum(number_fatalities)) %>%
   ggplot(aes(x = year, y = sum_fatal_state_yr)) +
@@ -78,7 +79,7 @@ fatal_unique %>%
   scale_x_continuous(breaks = seq(1989, 2020, 1), limits = c(1989, 2020)) + 
   facet_wrap( ~ state, scales = "free_y", ncol = 2) +
   labs(x = "Year", y = "Number of Fatalities",
-       title = 'Australian Road Fatalities by State in March (1989-2020)',
+       title = 'Australian Road Fatalities by State in April (1989-2020)',
        caption = "Source: @GrantChalmers | Australian Road Deaths Database (ARDD)") +
   theme(plot.title = element_text(size = 12, face = "bold"),
         plot.subtitle = element_text(size = 10, color = "gray20"),
